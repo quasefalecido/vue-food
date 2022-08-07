3e6c9f9ea40ea5c3a4239ae7120fe1bc974c9111
almapenada <rafael@unitronica.com.br>
2022-08-07 13:49:48 -0300

    install axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
+require('./bootstrap/index')
 import Vue from 'vue'
 import BaseTemplate from './layouts/BaseTemplate.vue'
 import router from './routes'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
     render: h => h(BaseTemplate),
-    router
+    router,
+    store
 }).$mount('#app')
